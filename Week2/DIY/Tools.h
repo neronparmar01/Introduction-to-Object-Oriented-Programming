23279e2a729a30d96bee2783d50f775ee61c2830
Neron Parmar <nparmar22@myseneca.ca>
2023-01-31 21:54:45 -0500

    Workshop 2....

---
+++ Week2/DIY/Tools.h
@@ -0,0 +1,25 @@
+/***********************************************************************
+// OOP244 Workshop 2 p2: tester program
+//
+// File  Tools.h
+// Version 1.1
+// Date   Fall 2022
+// Author Yosef Ashibani
+// Description
+//
+// Revision History
+// -----------------------------------------------------------
+// Name                  Date          Reason
+// Parmar Neron Nelson   1/31/23       reordered user's tweets report
+/////////////////////////////////////////////////////////////////
+***********************************************************************/
+
+#ifndef SDDS_TOOLS_H_
+#define SDDS_TOOLS_H_
+
+namespace sdds {
+	void strCpy(char* des, const char* src);
+}
+#endif 
+
+
